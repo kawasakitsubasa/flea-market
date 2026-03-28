c3e19be82c702668d300a960581c229b3048d5a9
kawasaki tsubasa <yic83470@gmail.com>
2026-03-28 11:01:33 +0900

    seeder編集

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -6,15 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run(): void
     {
-    $this->call([
-        ProductSeeder::class,
-    ]);
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class,
+        ]);
     }
 }
